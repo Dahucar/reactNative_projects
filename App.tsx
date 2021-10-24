0a42c2645df05ce4b9510ff108ba3e2c2b8029e1
Daniel HC <danielhuenul90@gmail.com>
2021-10-24 18:07:25 -0300

    proyecto base

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View >
+      <Text>
+        Hello
+      </Text>
+    </View>
+  );
+};
+
+export default App;
